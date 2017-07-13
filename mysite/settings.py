68ea3b6d0c205fa88b64e3b6956ae7f118e08c50
Romy <100romypeg@gmail.com>
2017-07-13 13:44:05 -0500

    second

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0ki3sa2#^6*741ftq_zfo_uk0az#9v@5k3n&sv&lya&4!%1p!+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '<romylevin>.pythonanywhere.com']
 
 
 # Application definition
